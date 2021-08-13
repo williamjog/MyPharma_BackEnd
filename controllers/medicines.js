ecea635924bce635611480974d7ee7d0e3ed77f9
William Gomes <williamjog@hotmail.com>
2021-08-13 19:32:20 -0300

    Adiciona endpoint post para cadastrar remedios

--- controllers/medicines.js
+++ controllers/medicines.js
@@ -6,6 +6,10 @@ const router = Router();
 
 const OK = 200;
 
+const CREATED = 201;
+
+const NON_AUTHORITATIVE_INFORMATION = 203;
+
 router.get('/', async (_request, response) => {
   try {
     const allMedicines = await connection('products').then((products) => products.find().toArray());
@@ -27,10 +31,18 @@ router.get('/:name/', async (request, response) => {
 });
 
 router.post('/', async (request, response) => {
-  const { name } = request.body;
-
-
-
+  try {
+    const { cod, name, description, price, stock } = request.body;
+    if (!cod || !name || !description || !price || !stock) {
+      return response.status(NON_AUTHORITATIVE_INFORMATION).json({ message: 'Invalid information.'});
+    }
+    await connection('products').then((products) => products.insertOne(
+      { cod, nome: name, descricao: description, preco: price, estoque: stock }
+    ));
+    return response.status(CREATED).json({ message: 'Medicine has been successfully created.'});
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 module.exports = router;
