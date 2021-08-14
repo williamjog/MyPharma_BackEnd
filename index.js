d41e5cf040b6d1e3b211e0a89c0b9fe0927f6f75
William Gomes <williamjog@hotmail.com>
2021-08-14 01:52:45 -0300

    testando express.static

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
@@ -10,6 +12,6 @@ app.use(express.json());
 
 app.use('/', medicines);
 
-app.use('/images', express.static('uploads'));
+app.use('/images', express.static(path.join(__dirname, 'uploads')));
 
 app.listen(process.env.PORT || 3000, () => console.log("Server has been started."));
