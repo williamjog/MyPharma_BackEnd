b77699f91c8f841c9f13363a789f683aaa024c5c
William Gomes <williamjog@hotmail.com>
2021-08-15 12:47:57 -0300

    Adiciona novos endpoints

--- controllers/medicines.js
+++ controllers/medicines.js
@@ -12,10 +12,24 @@ const router = Router();
 const OK = 200;
 const CREATED = 201;
 
-router.get('/', async (_request, response) => {
+router.get('/search', async (request, response) => {
+  const searchTerm = request.query.q;
+  const searchTermWithoutPlusSimbols = searchTerm.replace(/\+/g, ' ');
   try {
-    const allMedicines = await connection('products').then((products) => products.find().toArray());
-    return response.status(OK).json(allMedicines);
+    const medicineDetailed = await connection('products').then((products) => products.find(
+      { apresentacao: { $in: [ new RegExp(searchTermWithoutPlusSimbols, 'i') ] } }).toArray());
+    return response.status(OK).json(medicineDetailed);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+router.get('/product/:cod', async (request, response) => {
+  const formatedCod = Number(request.params.cod);
+  try {
+    const medicineDetailed = await connection('products').then((products) => products.findOne(
+      { cod: formatedCod }, {}));
+    return response.status(OK).json(medicineDetailed);
   } catch (err) {
     console.error(err.message);
   }
@@ -26,6 +40,15 @@ router.get('/:name/', async (request, response) => {
     const medicineDetailed = await connection('products').then((products) => products.findOne(
       { nome: request.params.name }, {}));
     return response.status(OK).json(medicineDetailed);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+router.get('/', async (_request, response) => {
+  try {
+    const allMedicines = await connection('products').then((products) => products.find().toArray());
+    return response.status(OK).json(allMedicines);
   } catch (err) {
     console.error(err.message);
   }
